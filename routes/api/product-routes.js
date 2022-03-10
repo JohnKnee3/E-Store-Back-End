57b336fd094e649391e5d2ec3181d80dbe1e78e4
john.a.clark3 <john.a.clark3@gmail.com>
2022-03-10 16:52:38 -0700

    went back and added id's to meet the requirements.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,11 +9,11 @@ router.get("/", (req, res) => {
     include: [
       {
         model: Category,
-        attributes: ["category_name"],
+        attributes: ["id", "category_name"],
       },
       {
         model: Tag,
-        attributes: ["tag_name"],
+        attributes: ["id", "tag_name"],
       },
     ],
   })
@@ -33,11 +33,11 @@ router.get("/:id", (req, res) => {
     include: [
       {
         model: Category,
-        attributes: ["category_name"],
+        attributes: ["id", "category_name"],
       },
       {
         model: Tag,
-        attributes: ["tag_name"],
+        attributes: ["id", "tag_name"],
       },
     ],
   })
