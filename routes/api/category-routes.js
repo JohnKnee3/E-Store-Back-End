6484815c03320b0ef209b8c784dc5f6f3bec8b74
john.a.clark3 <john.a.clark3@gmail.com>
2022-03-11 10:03:06 -0700

    edit tag done

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -46,8 +46,8 @@ router.get("/:id", (req, res) => {
     });
 });
 
+// create a new category
 router.post("/", (req, res) => {
-  // create a new category
   Category.create({
     category_name: req.body.category_name,
   })
@@ -58,8 +58,29 @@ router.post("/", (req, res) => {
     });
 });
 
+// update a category by its `id` value
 router.put("/:id", (req, res) => {
-  // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((dbCategoryData) => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: "No category found with this id" });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete("/:id", (req, res) => {
