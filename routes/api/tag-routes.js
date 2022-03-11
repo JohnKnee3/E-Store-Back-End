21577a09bceac8def8767ea7f9ee9c7ed926acb4
john.a.clark3 <john.a.clark3@gmail.com>
2022-03-11 09:30:53 -0700

    delete tag done

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -46,8 +46,8 @@ router.get("/:id", (req, res) => {
     });
 });
 
+// create a new tag
 router.post("/", (req, res) => {
-  // create a new tag
   Tag.create({
     tag_name: req.body.tag_name,
   })
@@ -58,8 +58,8 @@ router.post("/", (req, res) => {
     });
 });
 
+// update a tag's name by its `id` value
 router.put("/:id", (req, res) => {
-  // update a tag's name by its `id` value
   Tag.update(
     {
       tag_name: req.body.tag_name,
@@ -85,6 +85,22 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbTagData) => {
+      if (!dbTagData) {
+        res.status(404).json({ message: "No tag found with this id" });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
